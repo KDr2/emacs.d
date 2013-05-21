218c38812ff927aacde37d467e718605f330c869
KDr2 <killy.draw@gmail.com>
2013-05-21 22:19:44 +0800

    elpa settings

--- kdr2/packages.el
+++ kdr2/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- elpa settings
 
-;; Copyright (C) 2012 KDr2 
+;; Copyright (C) 2012 KDr2
 
 ;; Author   : KDr2 <killy.draw@gmail.com>
 ;; URL      : https://github.com/KDr2/k.emacs.d
@@ -9,13 +9,22 @@
 
 ;; This file is not part of GNU Emacs.
 ;;
+(require 'package)
 
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
-
-;; elnode
-(add-to-list 
-   'package-archives
-   '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;(add-to-list 'package-archives
+;;             '("ELPA" . "http://tromey.com/elpa/"))
 
 
+(package-initialize)
+
+;; Clojure Mode
+(unless (package-installed-p 'clojure-mode)
+  (package-refresh-contents)
+  (package-install 'clojure-mode))
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(when (not (package-installed-p 'nrepl))
+  (package-install 'nrepl))
+(setq nrepl-hide-special-buffers t)
