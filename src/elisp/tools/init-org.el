6e3b9a6f639498ffc3a02e6374df07be05cfe79b
KDr2 <killy.draw@gmail.com>
2014-03-02 20:17:20 +0800

    move kdr2-com setting to kdr2-on-web

--- src/elisp/tools/init-org.el
+++ src/elisp/tools/init-org.el
@@ -75,41 +75,8 @@
                 (insert (format "[[%s.png]]" basename basename)))
             (insert (format "[[%s.png][%s]]" basename title basename)))))))
 
-(defvar kdr2-com-html-head-extra
-  "
-<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS feed for KDr2\" href=\"http://kdr2.com/site-log.xml\">
-<script src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script>
-<script src=\"/script/site.js\"></script>")
-
-(defvar kdr2-com-html-preamble
-  "<div class='nav'>
-<ul>
-<li id=\"site-name\"></li>
-<li><a href='/'>Home</a></li>
-<li><a target='_blank' href='http://github.com/KDr2'>GitHub</a></li>
-<li><a target='_blank' href='http://kdr2.net'>Tumblr</a></li>
-<li><a href='/about.html'>About</a></li>
-<li class=\"search\">
-<form method=\"get\" action=\"http://www.google.com/search\">
-  <input type=\"text\" name=\"q\" size=\"31\" maxlength=\"255\" value=\"\" />
-  <input type=\"hidden\" id=\"sitesearch\" name=\"sitesearch\" value=\"kdr2.com\" />
-  <input type=\"submit\" value=\"Search\" class=\"button\" />
-</form>
-</li>
-</ul>
-</div>")
-
-(defvar kdr2-com-html-postamble
-  "
-<p>Copyright &copy; %a, <a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/\">SOME RIGHTS RESERVED</a>. </p>
-<p>Last updated: %C. </p>
-<p>Built with %c. </p>
-")
-
 (let ((kb-output-dir (vars-get 'org-publish-dir))
-      (kb-source-dir (concat org-directory "/kbuildup"))
-      (kdr2-com-output-dir (concat (vars-get 'work-dir) "/mine/kdr2-on-web/output"))
-      (kdr2-com-source-dir (concat (vars-get 'work-dir) "/mine/kdr2-on-web")))
+      (kb-source-dir (concat org-directory "/kbuildup")))
   (setq org-publish-project-alist
         `(("kb-org"
            :base-directory ,kb-source-dir
@@ -127,37 +94,7 @@
            :recursive t
            :publishing-function (org-publish-attachment)
            )
-          ("kb" :components ("kb-org" "kb-static"))
-          ("kdr2-com-org"
-           :base-directory ,kdr2-com-source-dir
-           :base-extension "org"
-           :publishing-directory ,kdr2-com-output-dir
-           :recursive t
-           :publishing-function (org-html-publish-to-html)
-           :html-head-extra ,kdr2-com-html-head-extra
-           :html-preamble ,kdr2-com-html-preamble
-           :html-postamble ,kdr2-com-html-postamble
-           :headline-levels 3
-           :auto-preamble t
-           :exclude "README.org"
-           )
-          ("kdr2-com-rss"
-           :base-directory ,kdr2-com-source-dir
-           :base-extension "org"
-           :publishing-directory ,kdr2-com-output-dir
-           :publishing-function (org-rss-publish-to-rss)
-           :html-link-home "http://kdr2.com/"
-           :html-link-use-abs-url t
-           :exclude ".*"
-           :include ("site-log.org"))
-          ("kdr2-com-static"
-           :base-directory ,(concat kdr2-com-source-dir "/static")
-           :base-extension "css\\|js\\|png\\|jpg\\|jpeg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|html"
-           :publishing-directory ,kdr2-com-output-dir
-           :recursive t
-           :publishing-function (org-publish-attachment)
-           )
-          ("kdr2-com" :components ("kdr2-com-org" "kdr2-com-rss" "kdr2-com-static")))))
+          ("kb" :components ("kb-org" "kb-static")))))
 
 (defun kb-pub ()
   (interactive)
