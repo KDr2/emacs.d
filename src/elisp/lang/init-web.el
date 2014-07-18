3779dfc32663e68f5a44ab73dfda28a5fb1620de
KDr2 <killy.draw@gmail.com>
2014-07-18 09:11:06 +0800

    web-mode for twig template

--- src/elisp/lang/init-web.el
+++ src/elisp/lang/init-web.el
@@ -22,6 +22,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
 
 
 (setq web-mode-markup-indent-offset 4)
