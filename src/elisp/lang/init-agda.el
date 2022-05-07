7bf3b6aa740376861672ae307b286af0bdcdea2e
KDr2 <zhuo.dev@gmail.com>
2022-05-07 21:50:18 +0800

    fix agda mode

--- src/elisp/lang/init-agda.el
+++ src/elisp/lang/init-agda.el
@@ -10,11 +10,11 @@
 
 ;; load agda mode
 
-(load-file (let* ((coding-system-for-read 'utf-8)
-                  (agda-mode-path (or (vars-get 'agda-dir)
-                                      (concat (getenv "HOME") "/.cabal/bin")))
-                  (agda-mode-exe (concat agda-mode-path "/agda-mode")))
-             (if (file-exists-p agda-mode-exe)
-                 (shell-command-to-string (concat agda-mode-exe " locate")))))
+(let* ((coding-system-for-read 'utf-8)
+       (agda-mode-path (or (vars-get 'agda-dir)
+                           (concat (getenv "HOME") "/.cabal/bin")))
+       (agda-mode-exe (concat agda-mode-path "/agda-mode")))
+  (if (file-exists-p agda-mode-exe)
+      (load-file (shell-command-to-string (concat agda-mode-exe " locate")))))
 
 (provide 'init-agda)
